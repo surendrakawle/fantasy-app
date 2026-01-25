53743aa4bbf72751381488fbc06f4c157713d7e1
return 0 <surendrakawle@MacBookAir.lan>
2026-01-25 23:27:50 +0530

    india

--- fantasy-backend/src/scripts/seed/players.ts
+++ fantasy-backend/src/scripts/seed/players.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import fs from "fs";
 import path from "path";
-import { Player } from "../models/Player.model";
+import { Player } from "../../models/Player.model";
 
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/fantasy";
 
